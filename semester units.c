afd8e1f1eb897a4e5123b333296964d83036d226
maxbrian-gii <maxbriankamau@gmail.com>
2026-09-13 13:22:18 +0300

    Add files via upload

---
+++ semester units.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+/*name: Maxbrian Kamau
+
+ reg no: CT100/G/30669/26
+ 
+ description: semester units program 
+ date:14/09/2026 */
+
+int main()
+{      printf("BSc IT UNITS SEM ONe\n");
+
+       printf("1.Fundamentals of Digital Technology and Artificial Intelligence\n");
+       
+       printf("2.Introduction to Computer Applications\n");
+       
+       printf("3.Essentials of Accounting\n");
+       
+       printf("4.Computer Systems and Organisation\n"); 
+       
+       printf("5.Mathematics for Science\n"); 
+        
+       printf("6.Introduction to Programing and Algorithms\n"); 
+       
+       printf("7.Communication and Information Literacy skills\n"); 
+       
+       return 0;  }
+
+
+
+
+    
